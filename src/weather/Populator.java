3ea6d6567a8528978511c947a243ce871d4fb0bf
Sean Salinas <ssalinas@nmt.edu>
2014-11-15 18:47:08 -0700

    Initial Commit. Added WeatherPoint and Main Populator.

---
+++ src/weather/Populator.java
@@ -0,0 +1,24 @@
+
+package weather;
+
+public class Populator
+{
+
+	public static void main(String[] args)
+	{
+		// Set up the connection to VoltDB
+		// TODO: Will
+
+		// Set up connection to NDFD
+		// TODO: Sean
+
+		// Pull data from NDFD
+		// TODO: Sean
+
+		// Add new data received from NDFD to voltDB
+		// TODO: Will
+
+		// Done
+	}
+
+}
---
+++ src/weather/WeatherPoint.java
@@ -0,0 +1,42 @@
+
+package weather;
+
+public class WeatherPoint
+{
+	// Temperature
+	public int maxTemp;
+	public int mintemp;
+	public int dewPointTemp;
+	public int heatIndexTemp;
+	public int windChillTemp;
+
+	// Precipitation
+	public int rainAmount;
+	public int snowAmount;
+
+	// Probability of Precipitation
+	public int probabilityOfPrecipitation;
+
+	// Convective Hazard
+	public int outlookPercent;
+	public int tornadoPercent;
+	public int hailPercent;
+	public int damagingThunderstormWindPercent;
+	public int extremeTornadoesPercent;
+	public int extremeHailPercent;
+	public int extremeThunderstormWindsPercent;
+	public int severeThunderstormPercent;
+	public int extremeSevereThunderstormPercent;
+
+	// Wind Speed
+	public int sustainedWindSpeed;
+	public int cumulative34WindSpeed;
+	public int gustWindSpeed;
+
+	// Wind Direction
+	public int windDirection;
+
+	// Cloud Amounts
+	public int cloudAmount;
+
+}
