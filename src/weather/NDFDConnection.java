b8fd5418565ae05acefc43751427fb5fc04f524c
Sean Salinas <ssalinas@nmt.edu>
2014-11-15 19:02:12 -0700

    Made NDFD connection class.

---
+++ src/weather/NDFDConnection.java
@@ -0,0 +1,19 @@
+
+package weather;
+
+import java.util.LinkedList;
+
+public class NDFDConnection
+{
+	public NDFDConnection()
+	{
+
+	}
+
+	public LinkedList<WeatherPoint> pullData()
+	{
+		LinkedList<WeatherPoint> newPoints = new LinkedList<WeatherPoint>();
+
+		return newPoints;
+	}
+}
