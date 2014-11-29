106b9c225bedbeccbf849ee4ce35218da07cfe33
Helumpago <wrosenberger@live.com>
2014-11-28 21:33:50 -0700

    Added method for inserting weatherpoint into db

--- src/TestClasses/TestConnection.java
+++ src/TestClasses/TestConnection.java
@@ -1,6 +1,7 @@
 package TestClasses;
 
 import voltDb.VoltDbConnection;
+import weather.WeatherPoint;
 
 /**
  * Simple test program for the VoltDB database
@@ -10,7 +11,20 @@ import voltDb.VoltDbConnection;
 public class TestConnection {
 
 	public static void main(String[] args) {
+		/* Open VoltDB connection */
+		VoltDbConnection conn = new VoltDbConnection("weatherpoints");
+		System.out.println("Opened connection to VoltDB");
 		
+		/* Create a weather point */
+		WeatherPoint wp = new WeatherPoint();
+		wp.cloudAmount = 50;
+		wp.maxTemp = 9001;
+		wp.mintemp = 42;
+		System.out.println("Built weatherpoint");
+		
+		/* Insert weather point into database */
+		conn.addRecord(wp);
+		System.out.println("Wrote weatherpoint to VoltDB");
 	}
 
 }
--- src/voltDb/VoltDbConnection.java
+++ src/voltDb/VoltDbConnection.java
@@ -2,6 +2,8 @@ package voltDb;
 
 import java.sql.*;
 
+import weather.WeatherPoint; 
+
 public class VoltDbConnection {
 	private static String driver = "org.voltdb.jdbc.Driver";
 	private static String url = "jdbc:voltdb://localhost:21212";
@@ -21,9 +23,49 @@ public class VoltDbConnection {
         	Class.forName(driver);
 			conn = DriverManager.getConnection(url);
 		} catch (SQLException e) {
-			System.out.println("Error creating JDBC connection");
+			System.err.println("Error creating JDBC connection");
 		} catch (ClassNotFoundException e) {
-			System.out.println("Error linking in VoltDB driver");
+			System.err.println("Error linking in VoltDB driver");
 		}
+	}
+	
+	/**
+	 * Inserts a new entry into the database
+	 * @param pt Data to insert
+	 */
+	public void addRecord(WeatherPoint pt) {
+		String sql  = "INSERT INTO " + tableName + "(maxTemp, mintemp, dewPointTemp, heatIndexTemp, windChillTemp, rainAmount, snowAmount, probabilityOfPrecipitation, outlookPercent, tornadoPercent, hailPercent, damagingThunderstormWindPercent, extremeTornadoesPercent, extremeHailPercent, extremeThunderstormWindsPercent, severeThunderstormPercent, extremeSevereThunderstormPercent, sustainedWindSpeed, cumulative34WindSpeed, gustWindSpeed, windDirection, cloudAmount)" + " VALUES (" +
+						+ pt.maxTemp + ", " +
+						+ pt.mintemp + ", " +
+						+ pt.dewPointTemp + ", " +
+						+ pt.heatIndexTemp + ", " +
+						+ pt.windChillTemp + ", " +
+						+ pt.rainAmount + ", " +
+						+ pt.snowAmount + ", " +
+						+ pt.probabilityOfPrecipitation + ", " +
+						+ pt.outlookPercent + ", " +
+						+ pt.tornadoPercent + ", " +
+						+ pt.hailPercent + ", " +
+						+ pt.damagingThunderstormWindPercent + ", " +
+						+ pt.extremeTornadoesPercent + ", " +
+						+ pt.extremeHailPercent + ", " +
+						+ pt.extremeThunderstormWindsPercent + ", " +
+						+ pt.severeThunderstormPercent + ", " +
+						+ pt.extremeSevereThunderstormPercent + ", " +
+						+ pt.sustainedWindSpeed + ", " +
+						+ pt.cumulative34WindSpeed + ", " +
+						+ pt.gustWindSpeed + ", " +
+						+ pt.windDirection + ", " +
+						+ pt.cloudAmount +
+						")";
+		System.out.println("Attempting to run following SQL query: " + sql);
+		
+		try {
+	        Statement query = conn.createStatement();
+	        query.executeUpdate(sql);
+        } catch (SQLException e) {
+        	System.err.println("Couldn't run SQL statement");
+        	e.printStackTrace();
+        }
 	}
 }
