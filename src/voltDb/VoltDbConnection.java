4562c312c7593a89996c5e81fb344bd5a1cce0cf
Helumpago <wrosenberger@live.com>
2014-11-28 20:10:40 -0700

    Added constructor for VoltDbConnection

--- src/voltDb/VoltDbConnection.java
+++ src/voltDb/VoltDbConnection.java
@@ -6,7 +6,24 @@ public class VoltDbConnection {
 	private static String driver = "org.voltdb.jdbc.Driver";
 	private static String url = "jdbc:voltdb://localhost:21212";
 	
-	public VoltDbConnection() {
+	private Connection conn = null;
+	private String tableName = null; // Table to use for SQL statements
+	
+	/**
+	 * Creates a new connection to the VoltDB server
+	 * @param tableName Name of table that this connection should execute statements on.
+	 */
+	public VoltDbConnection(String tableName) {
+		this.tableName = tableName;
 		
+		/* Load driver and set up database connection */
+        try {
+        	Class.forName(driver);
+			conn = DriverManager.getConnection(url);
+		} catch (SQLException e) {
+			System.out.println("Error creating JDBC connection");
+		} catch (ClassNotFoundException e) {
+			System.out.println("Error linking in VoltDB driver");
+		}
 	}
 }
