ea5403f512541e0c9d56a262090e6ee9f116b035
Sean Salinas <naeslas@gmail.com>
2014-11-30 01:02:43 -0700

    Working through the REST api.

--- src/weather/NDFDConnection.java
+++ src/weather/NDFDConnection.java
@@ -2,12 +2,28 @@ package weather;
 
 import java.util.LinkedList;
 
-import javax.xml.soap.*;
-
 public class NDFDConnection
 {
 	public NDFDConnection()
 	{
+		// This will make a new connection to the NDFD database.
+
+		// Start and End times to pull for.  Note, this has to be in the future (because it's a forecast database)
+		String beginTime = "2014-12-01T00:00:00";
+		String endTime = "2014-12-02T00:00:00";
+
+		// This is the list of zip codes that we want to pull.
+		// Specify multiple with the + operator. For instance, 87110+87111+87112
+		String zipcodeList = "87801";
+
+		String[] parameters = { "maxt", "mint", "dew" };
+		String requestURL = "http://graphical.weather.gov/xml/sample_products/browser_interface/ndfdXMLclient.php?zipCodeList=" + zipcodeList + "&product=time-series&begin=" + beginTime + "&end=" + endTime;
+		for (String s: parameters)
+		{
+			requestURL = requestURL.concat("&" + s + "=" + s);
+		}
+
+		// At this point, the request URL is ready.
 
 	}
 
