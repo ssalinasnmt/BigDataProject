f6e4926e51573cb17d4b9d962a2d7425db3dc018
Sean Salinas <naeslas@gmail.com>
2014-12-03 23:52:42 -0700

    Cleaned up formatting on weather point printing.

--- src/weather/WeatherPoint.java
+++ src/weather/WeatherPoint.java
@@ -33,28 +33,28 @@ public class WeatherPoint
 		// This prints out all data stored in a weather point.
 
 		System.out.println("Printing weather point.");
-		System.out.println("\tMax Temperature: " + maxTemp);
-		System.out.println("\tMin Temperature: " + minTemp);
-		System.out.println("\tDew Point Temperature: " + dewPoint);
-		System.out.println("\tPrecipitation Probability for 12 hour: " + precipitationProbability12hour);
-		System.out.println("\tLiquid Precipitation Amount (in hundreths of an inch): " + liquidPrecipitationAmount);
-		System.out.println("\tSnowfall Amount (in hundreths of an inch): " + snowfallAmount);
-		System.out.println("\tCloud Cover Amount: " + cloudCoverAmount);
-		System.out.println("\tRelative Humidity: " + relativeHumidity);
-		System.out.println("\tWind Speed: " + windSpeed);
-		System.out.println("\tWind Direction: " + windDirection);
-		System.out.println("\tWind Gust Speed: " + windGustSpeed);
-		System.out.println("\tProbability of Tornadoes: " + probabilityTornado);
-		System.out.println("\tProbability of Hail: " + probabilityHail);
-		System.out.println("\tProbability of Damaging Thunderstorm Winds" + probabilityDamagingThunderstormWinds);
-		System.out.println("\tProbability of Extreme Tornadoes: " + probabilityExtremeTornadoes);
-		System.out.println("\tProbability of Extreme Hail" + probabilityExtremeHail);
-		System.out.println("\tProbability of Extreme Thunderstorm Winds: " + probabilityExtremeThunderstormWinds);
-		System.out.println("\tProbability of Severe Thunderstorm Winds: " + probabilitySevereThunderstorm);
-		System.out.println("\tProbability of Extreme Severe Thunderstorm Winds" + probabilityExtremeThunderstormWinds);
-		System.out.println("\tProbability of Extreme Hyper Severe Intense Crazy Over 9000 Damaging Severe Extreme Ultra Thunderstorm Winds: 0 (we hope)");
-		System.out.println("\tMaximum Relative Humidity: " + maxRelativeHumidity);
-		System.out.println("\tMinimum Relative Humidity: " + minRelativeHumidity);
+		System.out.println("\tMax Temperature: " + maxTemp+" degrees");
+		System.out.println("\tMin Temperature: " + minTemp+" degrees");
+		System.out.println("\tDew Point Temperature: " + dewPoint+" degrees");
+		System.out.println("\tPrecipitation Probability for 12 hour: " + precipitationProbability12hour+"%");
+		System.out.println("\tLiquid Precipitation Amount: " + liquidPrecipitationAmount+" hundreths of an inch");
+		System.out.println("\tSnowfall Amount: " + snowfallAmount+" hundreths of an inch");
+		System.out.println("\tCloud Cover Amount: " + cloudCoverAmount+"%");
+		System.out.println("\tRelative Humidity: " + relativeHumidity+"%");
+		System.out.println("\tWind Speed: " + windSpeed+" miles per hour");
+		System.out.println("\tWind Direction: " + windDirection+" degrees");
+		System.out.println("\tWind Gust Speed: " + windGustSpeed+" miles per hour");
+		System.out.println("\tProbability of Tornadoes: " + probabilityTornado+"%");
+		System.out.println("\tProbability of Hail: " + probabilityHail+"%");
+		System.out.println("\tProbability of Damaging Thunderstorm Winds: " + probabilityDamagingThunderstormWinds+"%");
+		System.out.println("\tProbability of Extreme Tornadoes: " + probabilityExtremeTornadoes+"%");
+		System.out.println("\tProbability of Extreme Hail: " + probabilityExtremeHail+"%");
+		System.out.println("\tProbability of Extreme Thunderstorm Winds: " + probabilityExtremeThunderstormWinds+"%");
+		System.out.println("\tProbability of Severe Thunderstorm Winds: " + probabilitySevereThunderstorm+"%");
+		System.out.println("\tProbability of Extreme Severe Thunderstorm Winds: " + probabilityExtremeThunderstormWinds+"%");
+		System.out.println("\tProbability of Extreme Hyper Severe Intense Crazy Over 9000 Damaging Severe Extreme Ultra Thunderstorm Winds: 0% (we hope)");
+		System.out.println("\tMaximum Relative Humidity: " + maxRelativeHumidity+"%");
+		System.out.println("\tMinimum Relative Humidity: " + minRelativeHumidity+"%");
 		System.out.println();
 	}
 }
