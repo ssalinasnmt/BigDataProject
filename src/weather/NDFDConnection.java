3b2943ba4544044d498de1fda7a7043dc0d6f3f8
Sean Salinas <naeslas@gmail.com>
2014-12-03 21:42:14 -0700

    Changed the dates, for testing.

--- src/weather/NDFDConnection.java
+++ src/weather/NDFDConnection.java
@@ -26,8 +26,8 @@ public class NDFDConnection
 		// This will make a new connection to the NDFD database.
 
 		// Start and End times to pull for.  Note, this has to be in the future (because it's a forecast database)
-		String beginTime = "2014-12-01T00:00:00";
-		String endTime = "2014-12-02T00:00:00";
+		String beginTime = "2014-12-04T00:00:00";
+		String endTime = "2014-12-04T01:00:00";
 
 		// This is the list of zip codes that we want to pull.
 		// Specify multiple with the + operator. For instance, 87110+87111+87112
